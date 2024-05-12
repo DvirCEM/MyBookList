249816491fdd1527564c213713b8e9d0148eecaa
DvirArazi <108592948+DvirArazi@users.noreply.github.com>
2024-05-12 09:19:30 +0300

    deleted favorites

--- Program.cs
+++ Program.cs
@@ -197,7 +197,6 @@ class User(string id, string username, string password)
   public string Id { get; set; } = id;
   public string Username { get; set; } = username;
   public string Password { get; set; } = password;
-  public int[] Favorites = [];
 }
 
 class Book(string title, string imageSource, string description)
